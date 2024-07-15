a25a3d3353ef47bafec3957700de163d558e6bca
flcn <jfalconpriv@gmail.com>
2024-07-15 12:18:04 -0400

    updated index.js API controller to include all routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,7 +1,21 @@
-const router = require('express').Router();
+// Purpose: API routes for the application
+// variables
+const express = require("express");
+const router = express.Router();
 
-const gameRoutes = require('./game-routes');
+// import routes
+const authRoutes = require("../../utils/auth.js");
+const userRoutes = require("./user-routes");
+const gameRoutes = require("./game-routes");
+const chatroomRoutes = require("./chatroom-routes");
+const messageRoutes = require("./message-routes.js");
 
-router.use('/games', gameRoutes);
+// use routes
+router.use("/auth", authRoutes);
+router.use("/users", userRoutes);
+router.use("/games", gameRoutes);
+router.use("/chatrooms", chatroomRoutes);
+router.use("/messages", messageRoutes);
 
+// export routes
 module.exports = router;
