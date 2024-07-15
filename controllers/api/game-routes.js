e02c02555e152d64568374a5dac61a5c886b35c6
Jakostein97 <jacques.floresgarcia@gmail.com>
2024-07-15 10:27:08 -0400

    Adds GET routes to Game

--- controllers/api/game-routes.js
+++ controllers/api/game-routes.js
@@ -1,10 +1,24 @@
-const router = require('express').Router();
-const Game = require('../../models/Game');
+const router = require("express").Router();
+const Game = require("../../models/Game");
+
+// GET a game
+router.get("/", async (req, res) => {
+  const gameData = await Game.findAll();
+  
+  return res.json(bookData);
+});
+
+// GET a game by ID
+router.get("/:id", async (req, res) => {
+  const gameData = await Game.findByPk(req.params.id);
+  
+  return res.json(gameData);
+});
 
 // CREATE a game
-router.post('/games', async (req, res) => {
-    const gameData = await Game.create(req.body);
-  
-    return res.json(gameData);
-  });
-  
+router.post("/games", async (req, res) => {
+  const gameData = await Game.create(req.body);
+
+  return res.json(gameData);
+});
+
