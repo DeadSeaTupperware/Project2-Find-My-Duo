246623131866b1258130422ca3b694b014472902
Jakostein97 <jacques.floresgarcia@gmail.com>
2024-07-15 11:45:15 -0400

    adds code in seed.js for gameData

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,12 +1,19 @@
 const sequelize = require('../config/connection');
 const { User } = require('../models');
+const { Game } = require('../models');
 
 const userData = require('./userData.json');
+const gameData = require('./gameData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
   await User.bulkCreate(userData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  await Game.bulkCreate(gameData, {
     individualHooks: true,
     returning: true,
   });
