115e28cb8f84e87930cae1615eac142cca73f85d
flcn <jfalconpriv@gmail.com>
2024-07-15 10:01:27 -0400

    added initial user-routes.js file

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,7 @@
+// Purpose: To handle the user routes for the API
+// variables
+const router = require("express").Router();
+const { User, Chatroom, Game, Message } = require("../../models");
+
+// exports
+module.exports = router;
