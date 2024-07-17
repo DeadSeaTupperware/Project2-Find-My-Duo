9273366372f8d4279dcab872894b1bd605e50ac0
Jongho Bae <jb4918@columbia.edu>
2024-07-17 12:58:14 -0400

    redirect routes

--- controllers/api/game-routes.js
+++ controllers/api/game-routes.js
@@ -8,25 +8,33 @@ router.get("/", async (req, res) => {
     const gameData = await Game.findAll();
     res.json(gameData);
   } catch (error) {
-    res.status(500).json({ error: 'Failed to retrieve games' });
+    res.status(500).json({ error: "Failed to retrieve games" });
   }
 });
 
 // GET a game by ID
 router.get("/:id", async (req, res) => {
   try {
-    const gameData = await Game.findByPk(req.params.id, {
-      include: [{ model: Chatroom }]
-    });
-    if (!gameData) {
-      return res.status(404).json({ error: 'Game not found' });
+    const allGameData = await Game.findAll();
+    const games = allGameData.map((game) => game.get({ plain: true }));
+    const set = new Set();
+    while (set.size < 4) {
+      set.add(games[Math.floor(Math.random() * games.length)]);
     }
 
-    const game = gameData.get({plain: true})
-    console.log(game)
-    res.render('chatboard', { game });
+    const randomGames = Array.from(set);
+    const gameData = await Game.findByPk(req.params.id, {
+      include: [{ model: Chatroom }],
+    });
+    if (!gameData) {
+      return res.status(404).json({ error: "Game not found" });
+    }
+
+    const game = gameData.get({ plain: true });
+    console.log(game);
+    res.render("chatboard", { game, randomGames });
   } catch (error) {
-    res.status(500).json({ error: 'Failed to retrieve the game' });
+    res.status(500).json({ error: "Failed to retrieve the game" });
   }
 });
 
@@ -36,7 +44,7 @@ router.post("/", async (req, res) => {
     const gameData = await Game.create(req.body);
     res.status(201).json(gameData);
   } catch (error) {
-    res.status(400).json({ error: 'Failed to create the game' });
+    res.status(400).json({ error: "Failed to create the game" });
   }
 });
 
@@ -55,11 +63,11 @@ router.put("/:id", async (req, res) => {
       }
     );
     if (!gameData[0]) {
-      return res.status(404).json({ error: 'Game not found' });
+      return res.status(404).json({ error: "Game not found" });
     }
     res.json(gameData);
   } catch (error) {
-    res.status(400).json({ error: 'Failed to update the game' });
+    res.status(400).json({ error: "Failed to update the game" });
   }
 });
 
@@ -70,11 +78,11 @@ router.delete("/:id", async (req, res) => {
       where: { id: req.params.id },
     });
     if (!gameData) {
-      return res.status(404).json({ error: 'Game not found' });
+      return res.status(404).json({ error: "Game not found" });
     }
-    res.json({ message: 'Game deleted' });
+    res.json({ message: "Game deleted" });
   } catch (error) {
-    res.status(500).json({ error: 'Failed to delete the game' });
+    res.status(500).json({ error: "Failed to delete the game" });
   }
 });
 
