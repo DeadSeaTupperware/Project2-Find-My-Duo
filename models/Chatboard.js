1840446c8fc461ed926db4449d723ad593597368
flcn <jfalconpriv@gmail.com>
2024-07-17 14:33:49 -0400

    updated Chatboard.js model

--- models/Chatboard.js
+++ models/Chatboard.js
@@ -5,5 +5,39 @@ const sequelize = require("../config/connection");
 
 class Chatboard extends Model {}
 
+Chatboard.init(
+  {
+    // define columns
+    // id column
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // chatboard name
+    chatboard_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // game id to associate chatboard with a game
+    game_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "game",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "chatboard",
+  }
+);
+
 // export Chatboard model
 module.exports = Chatboard;
