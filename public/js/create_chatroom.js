41b8761e4a9f15e914b4ff3141c0deaacdbad193
Dean Bloomingdale <dean.bloomingdale1@gmail.com>
2024-07-17 21:10:25 -0400

    started createChatFormHandler layout

--- public/js/create_chatroom.js
+++ public/js/create_chatroom.js
@@ -1,9 +1,24 @@
 const createChatFormHandler = async (event) => {
   event.preventDefault();
 
-  const gameId = 
-  const chatroomName =
-  const
+  const gameId = document.querySelector("").value.trim();
+  const chatroomName = document.querySelector("").value.trim();
+  const chatroomPassword = document.querySelector("").value.trim();
+
+  if () {
+    const response = await fetch("/api/users", {
+      method: "POST",
+      body: JSON.stringify({ gameId, chatroomName, chatroomPassword }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/");
+    } else {
+      alert(response.statusText);
+      console.log(response.status);
+    }
+  }
 };
 
 document
